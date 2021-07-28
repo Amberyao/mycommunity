731f1f6222b64b358a4a3fd2216699669b0198ab
Amberyao <357782955@qq.com>
2021-07-28 09:32:00 +0800

    完成分页功能

--- src/main/java/com/amber/mycommunity/controller/IndexController.java
+++ src/main/java/com/amber/mycommunity/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.amber.mycommunity.controller;
 
+import com.amber.mycommunity.dto.PaginationDTO;
 import com.amber.mycommunity.dto.QuestionDTO;
 import com.amber.mycommunity.mapper.QuestionMapper;
 import com.amber.mycommunity.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +31,12 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public  String index(HttpServletRequest request, HttpServletResponse response, Model model){
+    public  String index(HttpServletRequest request, HttpServletResponse response, Model model,
+                         @RequestParam(name = "page", defaultValue = "1") Integer page,
+                         @RequestParam(name = "size", defaultValue = "5") Integer size,
+                         @RequestParam(name = "search", required = false) String search,
+                         @RequestParam(name = "tag", required = false) String tag,
+                         @RequestParam(name = "sort", required = false) String sort){
         Cookie[] cookies = request.getCookies();
         if (cookies!=null&&cookies.length!=0){
             for (Cookie cookie : cookies) {
@@ -43,8 +50,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList= questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO paginationDTO= questionService.list(page,size);
+        model.addAttribute("pagination",paginationDTO);
 
         return "index";
     }
--- src/main/java/com/amber/mycommunity/service/QuestionService.java
+++ src/main/java/com/amber/mycommunity/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.amber.mycommunity.service;
 
+import com.amber.mycommunity.dto.PaginationDTO;
 import com.amber.mycommunity.dto.QuestionDTO;
 import com.amber.mycommunity.mapper.QuestionMapper;
 import com.amber.mycommunity.mapper.UserMapper;
@@ -24,9 +25,26 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list(){
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size){
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount= questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+
+        if (page<1){
+            page=1;
+        }
+        if (page>paginationDTO.getTotalPage()){
+            page=paginationDTO.getTotalPage();
+        }
+
+
+        Integer offset =size*(page-1);
+
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList=new ArrayList<>();
+
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -34,6 +52,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
+
 }
